4640b897433308e6b4cd3f893054e08556762e1f
thegi.code <thegi.code@gmail.com>
2021-12-06 14:22:26 +0900

    [graphs] Update polyline-3 , data가 null 인 경우

--- graphs/src/js/polyline3.js
+++ graphs/src/js/polyline3.js
@@ -1,6 +1,6 @@
 
 // 임시 데이터
-const DATA = [50, 20, 60, 40, 80, 70, 90]
+const DATA = [50, null, null, 40, 80, 70, 90]
 
 const rootEl = document.querySelector('.root'),
     svgEl = rootEl.querySelector('svg'),
@@ -14,16 +14,22 @@ const xLen = DATA.length,
     xUnit = width / xLen
 
 const pointsArr = DATA.map( (point, index) => {
-    const x = Math.round(xUnit * index + xUnit/2),
-        y = height - point/100 * height
+    
     const pointEl = pointsEl[index]
+
+    if (point === null) {
+        pointEl.style.display = 'none'
+        return ''
+    }
+    
+    const x = Math.round(xUnit * index + xUnit/2)
+    const y = height - point/100 * height
     pointEl.style.top = `${y}px`
     pointEl.style.left = `${x}px`
     return `${x} ${y}`
 })
 
 polylineEl.setAttribute('points', pointsArr.join(' '))
-
 
 document.querySelector('.log span')
     .textContent = DATA.join(' ')
