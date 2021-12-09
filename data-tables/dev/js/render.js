fe75e87e236e34caa7db58fc7ef56a9f47735ab2
thegi.code <thegi.code@gmail.com>
2021-12-09 16:10:31 +0900

    [data-tables] Update render: requestAnimation check

--- data-tables/dev/js/render.js
+++ data-tables/dev/js/render.js
@@ -1,9 +1,9 @@
 
 const render = (rootEl, store) => {
-    const tbodyEl = rootEl.querySelector('tbody'),
-        tfootEl = rootEl.querySelector('tfoot')
+    const tbodyEl = rootEl.querySelector('tbody')
 
     const sum = () => {
+        const tfootEl = rootEl.querySelector('tfoot')
         const storeData = store.data
         const sum = {
             _amount: 0,
@@ -18,13 +18,12 @@ const render = (rootEl, store) => {
                 sum._discountPrice +=  data.discountPrice
             }
         })
-        window.requestAnimationFrame( () => {
-            sum._total = sum._price - sum._discountPrice
-            tfootEl.querySelector('.amount').textContent = sum._amount.toLocaleString()
-            tfootEl.querySelector('.price').textContent = sum._price.toLocaleString()
-            tfootEl.querySelector('.discount').textContent = sum._discountPrice.toLocaleString()
-            tfootEl.querySelector('.total').textContent = sum._total.toLocaleString()
-        })
+        sum._total = sum._price - sum._discountPrice
+
+        tfootEl.querySelector('.amount').textContent = sum._amount.toLocaleString()
+        tfootEl.querySelector('.price').textContent = sum._price.toLocaleString()
+        tfootEl.querySelector('.discount').textContent = sum._discountPrice.toLocaleString()
+        tfootEl.querySelector('.total').textContent = sum._total.toLocaleString()
     }
 
     const table = (getNode) => {
@@ -34,10 +33,23 @@ const render = (rootEl, store) => {
             const node = getNode(data, index)
             fragment.appendChild(node)
         })
-        window.requestAnimationFrame( () => {
+        // window.requestAnimationFrame( () => {
+            // console.time('a')
             tbodyEl.appendChild(fragment)
             sum()
-        })
+            // console.timeEnd('a')
+        // })
+
+        // requestAnimationFrame
+        // load : 1.31787109375, 0.636962890625, 1.977783203125
+        // delete center : 0.231201171875, 1.129150390625, 1.551025390625
+
+        // normal
+        // load: 0.461181640625, 0.525146484375, 1.32080078125
+        // delete center: 0.32080078125, 2.466064453125, 0.294921875
+
+        // load : normal?
+        // delete : requestAnimationFrame
     }
 
     const discountPrice = (el) => {
