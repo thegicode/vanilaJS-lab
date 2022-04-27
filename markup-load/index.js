8a6af2e97e3f95d738e233e0db4b4eff0b02293c
thegicode <thegi.code@gmail.com>
2022-04-27 11:55:28 +0900

    [markup-load] Add write components html

--- markup-load/index.js
+++ markup-load/index.js
@@ -1,25 +1,38 @@
 const fs = require('fs');
 
-const utfCode = utfCode
+const utfCode = 'utf8'
 
 const path = {
-    content: './htmls/content/',
     template: './htmls/template/',
+    components: './htmls/components/',
+    content: './htmls/content/',
     layout: './htmls/layout/',
     page: './htmls/page/',
 }
 
+
 // Get template string
-const getTemplateString = (templateNames) => {
-    const templateArr = templateNames.map( name => {
+const getTemplateString = (names) => {
+    const templateArr = names.map( name => {
         const url = `${path.template}${name}.html`
         return fs.readFileSync(url, utfCode)
     })
     return templateArr.join('\n')
 }
 
+// Get components string
+const getComponents = (names) => {
+    const obj = {}
+    names.forEach( name => {
+        const url = `${path.components}${name}.html`
+        const str = fs.readFileSync(url, utfCode)
+        obj[name] = str
+    })
+    return obj
+}
+
 // Make page file func
-const writePage = (pageName, templateNames) => {
+const writePage = (pageName, templateNames, componentNames) => {
     try {
         const fileName = `${pageName}.html`
         const contentUrl = `${path.content}${fileName}`
@@ -27,7 +40,13 @@ const writePage = (pageName, templateNames) => {
         const pageUrl = `${path.page}${fileName}`
         
         const templateStr = getTemplateString(templateNames)
-        const contentStr = fs.readFileSync(contentUrl, utfCode)
+        let contentStr = fs.readFileSync(contentUrl, utfCode)
+
+        const componentObj = getComponents(componentNames)
+        for (const [key, value] of Object.entries(componentObj)) {
+            contentStr = contentStr.replace(`{${key}}`, value)
+        }
+
         const layoutStr = fs.readFileSync(layoutUrl, utfCode)
     
         let str = `${contentStr}\n${templateStr}`
@@ -42,12 +61,15 @@ const writePage = (pageName, templateNames) => {
 // Page names, template names
 const pages = {
     main: {
-        templateNames: ['template', 'template2']
+        templateNames: ['template', 'template2'],
+        componentNames: ['component1', 'component2'],
     },
     list: {
-        templateNames: ['template', 'template3']
+        templateNames: ['template', 'template3'],
+        componentNames: ['component1', 'component2'],
     }
 }
 for (const [key, value] of Object.entries(pages)) {
-    writePage(key, value.templateNames)
+    writePage(key, value.templateNames, value.componentNames)
 }
+
