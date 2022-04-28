edc70fa62530ecd0a8d1fd311198313c887c5904
thegicode <thegi.code@gmail.com>
2022-04-28 16:11:19 +0900

    [markup-load-front] Add remove space

--- markup-load-front/src/script/template.js
+++ markup-load-front/src/script/template.js
@@ -2,25 +2,7 @@ const getDomContent = (htmlStr) => {
     const parser = new DOMParser()
     const doc = parser.parseFromString(htmlStr, "text/html")
     return doc.querySelector('template').content.firstElementChild.cloneNode(true)
-} 
-
-export const template1 = getDomContent(`<template>
-    <div>
-        <p>template1 content</p>
-        <button>click</button>
-        <p></p>
-    </div>
-</template>`)
-
-export const template2 = getDomContent(`<template>
-    <li>
-        <a href="">template2 content</a>
-        <p>222</p>
-    </li>
-</template>`)
-
-export const template3 = getDomContent(`<template>
-    <div>
-        <p>template3 content</p>
-    </div>
-</template>`)
+}
+export const template1 = getDomContent('<template><div><h1>title</h1><p>template1 content</p><button>click</button>{ eigiege }<p></p></div></template>')
+export const template2 = getDomContent('<template><li><h3>title</h3><a href="">template2 content</a><p>222</p><span>card</span></li></template>')
+export const template3 = getDomContent('<template><div><p>template3 content</p></div></template>')
--- markup-load-front/template-to-js.js
+++ markup-load-front/template-to-js.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const path = require('path')
 
 const utfCode = 'utf8'
-
 const PATH = {
     TEMPLATE: './src/template/',         
     SCRIPT: './src/script/'             
@@ -13,21 +12,21 @@ try {
     const files = fs.readdirSync(directory)
     const templateArr = files.map( fileName => {
         const url = path.join(directory, fileName)
-        const htmlStr = fs.readFileSync(url, utfCode)
+        const htmlStr = fs.readFileSync(url, utfCode).replace(/$\s+/gm, '')
         const name = fileName.replace('.html', '')
-        return `export const ${name} = ${"getDomContent(`" + htmlStr + "`)"}`
+        return `export const ${name} = getDomContent('${htmlStr}')`
     })
 
     let result = `const getDomContent = (htmlStr) => {
     const parser = new DOMParser()
     const doc = parser.parseFromString(htmlStr, "text/html")
     return doc.querySelector('template').content.firstElementChild.cloneNode(true)
-} \n\n`
-    result += templateArr.join('\n\n')
+}\n`
 
-    const jsUrl = `${PATH.SCRIPT}template.js`
+    result += templateArr.join('\n')
+
+    const jsUrl = path.join(PATH.SCRIPT, 'template.js')
     fs.writeFileSync(jsUrl, result)
-    // console.log(result)
 
 } catch(err) {
     console.error(err)
